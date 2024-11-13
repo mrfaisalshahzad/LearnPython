8c6a7600d8daba73401289df83c8953b8d778a2d
mr.faisalshahzad <mr.faisalshahzad@hotmail.com>
2024-11-13 03:25:05 +0300

    No change

--- Modules.py
+++ Modules.py
@@ -5,5 +5,17 @@ sqr = math.sqrt(16)
 print(sqr)
 """
 
+"""
+import Functions_for_Modules
+import Functions_for_Modules as f
+
+print(Functions_for_Modules.calc_square_area(5))
+"""
+
+from Functions_for_Modules import *
+print(calc_square_area(5))
+print(calc_triangle_area(5,3))
+
+
 
 
