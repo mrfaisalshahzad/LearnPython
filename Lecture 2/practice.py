f39dcacb6129dd1a997ca60473ec533b97776a58
Faisal Shahzad <119738031+mrfaisalshahzad@users.noreply.github.com>
2024-12-28 09:47:35 +0300

    practice

---
+++ Lecture 2/practice.py
@@ -0,0 +1,9 @@
+#get name input and print it and its length
+first_name = input("Enter First Name: ")
+print("First Name: ", first_name)
+print("length: ", len(first_name))
+
+
+#find count of $ in a string
+Amount = "Amount in $: "
+print(Amount.count("$"))
